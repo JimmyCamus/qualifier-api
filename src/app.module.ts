f0417b0c6b111b3e90b295ff412f640a653c89e2
Jimmy Camus <jeremy.camus.varela@gmail.com>
2022-12-28 00:09:37 -0300

    CI: config module was added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URL)],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+  ],
 })
 export class AppModule {}
