18d334210c3920f9c1068ef482e9feeb9c6c1ef4
Jimmy Camus <jeremy.camus.varela@gmail.com>
2022-12-28 16:35:29 -0300

    feat: jwt authorization was implemented

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,6 +8,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from 'src/users/schemas/user.schema';
 import { UsersService } from 'src/users/services/users.service';
 import { LocalStrategy } from './strategies/local.auth';
+import { JwtStrategy } from './strategies/jwt.auth';
 
 @Module({
   imports: [
@@ -19,7 +20,7 @@ import { LocalStrategy } from './strategies/local.auth';
     }),
     MongooseModule.forFeature([{ name: 'user', schema: UserSchema }]),
   ],
-  providers: [AuthService, UsersService, LocalStrategy],
+  providers: [AuthService, UsersService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
