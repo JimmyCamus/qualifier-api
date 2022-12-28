d8fda104da31d3629319f74943842c1e484dc332
Jimmy Camus <jeremy.camus.varela@gmail.com>
2022-12-28 12:48:45 -0300

    fix: dto validation was fixed

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { UserDto } from '../dto/user.dto';
 import { UserDocument } from '../interfaces/user.document';
 import { UsersService } from '../services/users.service';
 
@@ -6,11 +7,10 @@ import { UsersService } from '../services/users.service';
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
   @Post('/signup')
-  async createUser(
-    @Body('username') username: string,
-    @Body('password') password: string,
-    @Body('email') email: string,
-  ): Promise<UserDocument> {
+  async createUser(@Body() body: UserDto): Promise<UserDocument> {
+    const username = body.username;
+    const password = body.password;
+    const email = body.email;
     return await this.userService.signup(username, password, email);
   }
 }
