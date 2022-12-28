c30c9b91f3a15d9432741fc2f9f6e7bcbea1fa95
Jimmy Camus <jeremy.camus.varela@gmail.com>
2022-12-28 15:58:26 -0300

    feat: login and user user verification was implemented

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './services/auth.service';
+import { AuthController } from './controllers/auth.controller';
+import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from 'src/users/schemas/user.schema';
+import { UsersService } from 'src/users/services/users.service';
+import { LocalStrategy } from './strategies/local.auth';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'secretKey',
+      signOptions: { expiresIn: '365d' },
+    }),
+    MongooseModule.forFeature([{ name: 'user', schema: UserSchema }]),
+  ],
+  providers: [AuthService, UsersService, LocalStrategy],
+  controllers: [AuthController],
+})
+export class AuthModule {}
