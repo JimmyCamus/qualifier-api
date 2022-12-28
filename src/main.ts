4ee10e6214761bd27e9e2bc8c6fb5b76bc465357
Jimmy Camus <jeremy.camus.varela@gmail.com>
2022-12-28 00:02:23 -0300

    CI: security config was added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.use(helmet());
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'x-recaptcha-authorization',
+    ],
+    methods: ['GET', 'DELETE', 'PUT', 'POST'],
+  });
+  await app.listen(8080);
 }
 bootstrap();
