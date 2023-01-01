0c9adb5f5940ad60e80ab9511812cc7591ef9889
Jimmy Camus <jeremy.camus.varela@gmail.com>
2023-01-01 16:56:22 -0300

    feat: user id is now in the jwt

--- src/auth/strategies/local.auth.ts
+++ src/auth/strategies/local.auth.ts
@@ -14,6 +14,6 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     if (!user) {
       throw new UnauthorizedException();
     }
-    return user;
+    return { username: user.username, email: user.email, id: user._id };
   }
 }
